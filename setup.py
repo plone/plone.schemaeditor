733ac764d04ff3c69e5bfcf8089ef35a70a02193
David Glick <davidglick@groundwire.org>
2008-10-11 16:56:18 +0000

    paster-generated template
    svn path=/plone.schemaeditor/trunk/; revision=22794

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='plone.schemaeditor',
+      version=version,
+      description="Provides through the web editing of a zope 3 schema/interface.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone schema ttw',
+      author='David Glick and Nathan van Gheem',
+      author_email='davidglick@onenw.org',
+      url='http://svn.plone.org/svn/plone/plone.schemaeditor',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
