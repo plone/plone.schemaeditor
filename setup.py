19e919198f31a0230fcea6fa2517c2083b6fc930
David Glick <davidglick@groundwire.org>
2008-10-12 04:48:16 +0000

    updated dependencies
    svn path=/plone.schemaeditor/trunk/; revision=22856

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ version = '0.1'
 
 setup(name='plone.schemaeditor',
       version=version,
-      description="Provides through the web editing of a zope 3 schema/interface.",
+      description="Provides through-the-web editing of a zope 3 schema/interface.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Zope2",
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone schema ttw',
-      author='David Glick and Nathan van Gheem',
+      author='David Glick and Nathan Van Gheem',
       author_email='davidglick@onenw.org',
       url='http://svn.plone.org/svn/plone/plone.schemaeditor',
       license='GPL',
@@ -25,6 +26,13 @@ setup(name='plone.schemaeditor',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          # Zope 2,
+          'zope.interface',
+          'zope.component',
+          'zope.schema',
+          'zope.publisher',
+          'z3c.form',
+          'plone.z3cform',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
