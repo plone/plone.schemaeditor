b36a88c3526378fd495839f631c79437d357c0b5
Ross Patterson <me@rpatterson.net>
2010-03-17 00:46:14 +0000

    Factor out the zope.schema filed schema corrections
    svn path=/plone.schemaeditor/trunk/; revision=35020

--- plone/schemaeditor/interfaces.py
+++ plone/schemaeditor/interfaces.py
@@ -1,7 +1,5 @@
 from zope.interface.interfaces import Interface, IInterface
 from zope.publisher.interfaces.browser import IBrowserPage
-from zope.schema import interfaces 
-from zope import schema
 from zope.schema import Object, TextLine
 from zope.schema.interfaces import IField
 from z3c.form.interfaces import IFieldWidget, IEditForm
@@ -66,28 +64,3 @@ class IFieldEditForm(IEditForm):
 class IMetaFieldWidget(IFieldWidget):
     """ Marker interface for a z3c.form widget that is a meta field widget.
     """
-
-class IText(interfaces.IText):
-
-    default = schema.Text(
-        title=interfaces.IText['default'].title,
-        description=interfaces.IText['default'].description,
-        required=False)
-
-    missing_value = schema.Text(
-        title=interfaces.IText['missing_value'].title,
-        description=interfaces.IText['missing_value'].description,
-        required=False)
-
-class ITextLine(interfaces.ITextLine):
-
-    default = schema.TextLine(
-        title=interfaces.ITextLine['default'].title,
-        description=interfaces.ITextLine['default'].description,
-        required=False)
-
-    missing_value = schema.TextLine(
-        title=interfaces.ITextLine['missing_value'].title,
-        description=interfaces.ITextLine['missing_value'].description,
-        required=False)
-    
---
+++ plone/schemaeditor/schema.py
@@ -0,0 +1,27 @@
+from zope.schema import interfaces 
+from zope import schema
+
+class IText(interfaces.IText):
+
+    default = schema.Text(
+        title=interfaces.IText['default'].title,
+        description=interfaces.IText['default'].description,
+        required=False)
+
+    missing_value = schema.Text(
+        title=interfaces.IText['missing_value'].title,
+        description=interfaces.IText['missing_value'].description,
+        required=False)
+
+class ITextLine(interfaces.ITextLine):
+
+    default = schema.TextLine(
+        title=interfaces.ITextLine['default'].title,
+        description=interfaces.ITextLine['default'].description,
+        required=False)
+
+    missing_value = schema.TextLine(
+        title=interfaces.ITextLine['missing_value'].title,
+        description=interfaces.ITextLine['missing_value'].description,
+        required=False)
+    
