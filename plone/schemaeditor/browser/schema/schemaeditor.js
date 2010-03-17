ffc7f0f7687aab86dec083208a1dc34037d52d54
Alexander Limi <limi@plone.org>
2010-03-17 23:10:25 +0000

    Adding pop-up support for field editing, improved UI in general
    svn path=/plone.schemaeditor/branches/davisagli-tahoe-ui/; revision=35193

--- plone/schemaeditor/browser/schema/schemaeditor.js
+++ plone/schemaeditor/browser/schema/schemaeditor.js
@@ -1,10 +1,25 @@
-jq(document).ready(function() {
-    jq("a.schemaeditor-delete-field").click(function(e){
+(function($){
+$(document).ready(function() {
+
+    $("a.schemaeditor-delete-field").click(function(e){
         e.preventDefault();
-        var trigger = jq(this);
-        jq.post(trigger.attr('href'), null, function (data) {
+        var trigger = $(this);
+        $.post(trigger.attr('href'), null, function (data) {
           trigger.closest('.fieldPreview').remove();
           }, 'text'
         );
     });
+
+    common_content_filter = '#content>*:not(div.configlet),dl.portalMessage.error,dl.portalMessage.info';
+
+    // field settings form
+    $('a.fieldSettings').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+        }
+    );
+
+
 });
+})(jQuery);
