bb4492df37c9a61cf424540b12c1a3d0e12d2fbb
David Glick <davidglick@groundwire.org>
2010-03-23 05:46:29 +0000

    implement field reordering using HTML 5 drag events (tested on FF and Safari)
    svn path=/plone.schemaeditor/branches/davisagli-tahoe-ui/; revision=35484

--- plone/schemaeditor/browser/schema/schemaeditor.js
+++ plone/schemaeditor/browser/schema/schemaeditor.js
@@ -1,4 +1,56 @@
 (function($){
+$.fn.plone_schemaeditor_html5_sortable = function(callback) {
+    this.attr('draggable', 'true')
+        .css('-webkit-user-drag', 'element')
+        .each(function (i) { $(this).attr('data-drag_id', i); })
+        .bind('dragstart', function(e) {
+            e.originalEvent.dataTransfer.setData('Text', $(this).attr('data-drag_id'));
+            $('<div id="drop-marker" style="position: absolute; width: 100%;"></div>').insertBefore(this);
+        })
+        .bind('dragenter', function(e) { return false; })
+        .bind('dragleave', function(e) { return false; })
+        .bind('dragover', function(e) {
+            var position = $(this).position();
+            var height = $(this).height();
+            var marker = $('#drop-marker');
+            marker.css('border-bottom', '5px dotted red');
+            if (e.pageY < ($(this).offset().top + height / 2)) {
+                marker.css('top', position.top + 4 + 'px');
+                $(this).attr('draghalf', 'top');
+            } else {
+                marker.css('top', position.top + height + 42 + 'px');
+                $(this).attr('draghalf', 'bottom');
+            }
+            // window autoscroll
+            if (!$('html,body').is(':animated')) {
+                if ($(window).scrollTop() + $(window).height() - e.pageY < 30) { // bottom
+                    $('html,body').animate({scrollTop: $(window).scrollTop() + 50}, 200);
+                } else if (e.pageY - $(window).scrollTop() < 30) { // top
+                    $('html,body').animate({scrollTop: $(window).scrollTop() - 50}, 200);
+                }
+            }
+            return false;
+        })
+        .bind('drop', function(e) {
+            e.preventDefault();
+            var src = e.originalEvent.dataTransfer.getData('Text');
+            var node = $('[data-drag_id=' + src + ']');
+            if ($(this).attr('data-drag_id') == src) return;
+            if ($(this).attr('draghalf') == 'top') {
+                node.insertBefore(this);
+            } else {
+                node.insertAfter(this);
+            }
+            callback.apply(node, [node.parent().children('[data-drag_id]').index(node)]);
+        })
+        .bind('dragend', function(e) {
+            $('#drop-marker').remove();
+        });
+    $('<span class="draghandle">&#x28FF;</span>')
+        .css('cursor', 'ns-resize')
+        .prependTo('.fieldLabel');
+}
+
 $(document).ready(function() {
 
     // delete field
@@ -12,6 +64,11 @@ $(document).ready(function() {
           trigger.closest('.fieldPreview').remove();
           }, 'text'
         );
+    });
+
+    // reorder fields
+    $('.fieldPreview').plone_schemaeditor_html5_sortable(function(i) {
+        $.post(window.location.href + '/' + this.attr('data-field_id') + '/@@order', {pos: i});
     });
 
     common_content_filter = '#content>*:not(div.configlet),dl.portalMessage.error,dl.portalMessage.info';
