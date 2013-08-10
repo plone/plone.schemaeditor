33b2d186f40c4f97840ac5c5bb27dff5a11810b1
Rok Garbas <rok@garbas.si>
2013-08-10 16:13:14 +0200

    using plone.supermodel instead of plone.directives.form

--- plone/schemaeditor/tests/fixtures.py
+++ plone/schemaeditor/tests/fixtures.py
@@ -1,15 +1,13 @@
 from zope.interface import Interface
 from zope import schema
 from plone.schemaeditor.browser.schema.traversal import SchemaContext
-from plone.directives import form
+from plone.supermodel import model
 from plone.supermodel.interfaces import FIELDSETS_KEY
 
 
 class IDummySchema(Interface):
 
-    form.fieldset('alpha',
-        fields=['fieldA']
-    )
+    model.fieldset('alpha', fields=['fieldA'])
 
     field1 = schema.TextLine()
     field2 = schema.TextLine()
@@ -30,16 +28,19 @@ def get_field_fieldset(field_id, schema):
 
 class DummySchemaContext(SchemaContext):
     def __init__(self, context, request):
-        super(DummySchemaContext, self).__init__(IDummySchema, request, name='@@schemaeditor')
+        super(DummySchemaContext, self).__init__(
+            IDummySchema, request, name='@@schemaeditor')
 
 
 def log_event(object, event):
-    print '[event: %s on %s]' % (event.__class__.__name__, object.__class__.__name__)
+    print '[event: %s on %s]' % (
+        event.__class__.__name__, object.__class__.__name__)
 
 
 from z3c.form import field
 from z3c.form.form import EditForm
 from plone.z3cform import layout
+
 
 class EditForm(EditForm):
 
